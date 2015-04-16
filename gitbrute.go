00011113d1e7d28996ed803148c3b91cbde97639
Kevin Wallace <kevin@pentabarf.net>
2015-04-16 16:42:44 -0700

    Add nonce rather than twiddling dates

--- gitbrute.go
+++ gitbrute.go
@@ -23,12 +23,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"regexp"
 	"runtime"
-	"strconv"
 	"strings"
 )
 
@@ -40,175 +38,22 @@ var (
 	re *regexp.Regexp
 )
 
-func main() {
-	flag.Parse()
-	runtime.GOMAXPROCS(*cpu)
-
-	hash := curHash()
-
-	var err error
-	re, err = regexp.Compile(*pattern)
-	if err != nil {
-		log.Fatalf("Pattern %q is not a valid regexp: %s", err)
-	}
-
-	if re.MatchString(hash) && !*force {
-		return
-	}
-
-	obj, err := exec.Command("git", "cat-file", "-p", hash).Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	i := bytes.Index(obj, []byte("\n\n"))
-	if i < 0 {
-		log.Fatalf("No \\n\\n found in %q", obj)
-	}
-	msg := obj[i+2:]
-
-	var maxCommitBehind int64 = math.MaxInt64
-	if parentObj, err := exec.Command("git", "cat-file", "-p", "HEAD^").Output(); err == nil {
-		d, _ := getDate(obj, commiterDateRx)
-		pd, _ := getDate(parentObj, commiterDateRx)
-		if d.n >= pd.n {
-			maxCommitBehind = d.n - pd.n
-		}
-	}
-
-	possibilities := make(chan try, 512)
-	go explore(possibilities, int(maxCommitBehind))
-
-	winner := make(chan solution)
-	done := make(chan struct{})
-
-	for i := 0; i < *cpu; i++ {
-		go bruteForce(obj, winner, possibilities, done)
-	}
-
-	w := <-winner
-	close(done)
-
-	cmd := exec.Command("git", "commit", "--amend", "--date="+w.author.String(), "--file=-")
-	cmd.Env = append([]string{"GIT_COMMITTER_DATE=" + w.committer.String()}, os.Environ()...)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = bytes.NewReader(msg)
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("amend: %v", err)
-	}
-}
-
-type solution struct {
-	author, committer date
-}
-
-var (
-	authorDateRx   = regexp.MustCompile(`(?m)^author.+> (.+)`)
-	commiterDateRx = regexp.MustCompile(`(?m)^committer.+> (.+)`)
-)
-
-func bruteForce(obj []byte, winner chan<- solution, possibilities <-chan try, done <-chan struct{}) {
-	// blob is the blob to mutate in-place repatedly while testing
-	// whether we have a match.
-	blob := []byte(fmt.Sprintf("commit %d\x00%s", len(obj), obj))
-	authorDate, adatei := getDate(blob, authorDateRx)
-	commitDate, cdatei := getDate(blob, commiterDateRx)
-
-	s1 := sha1.New()
-	hexBuf := make([]byte, 0, sha1.Size*2)
-
-	for t := range possibilities {
-		select {
-		case <-done:
-			return
-		default:
-			ad := date{authorDate.n - int64(t.authorBehind), authorDate.tz}
-			cd := date{commitDate.n - int64(t.commitBehind), commitDate.tz}
-			strconv.AppendInt(blob[:adatei], ad.n, 10)
-			strconv.AppendInt(blob[:cdatei], cd.n, 10)
-			s1.Reset()
-			s1.Write(blob)
-			if !re.Match(hexInPlace(s1.Sum(hexBuf[:0]))) {
-				continue
-			}
-
-			winner <- solution{ad, cd}
-			return
-		}
-	}
-}
-
-// try is a pair of seconds behind now to brute force, looking for a
-// matching commit.
+// try is a nonce to brute force, looking for a matching commit.
 type try struct {
-	commitBehind int
-	authorBehind int
+	nonce int
 }
 
-// explore yields the sequence:
-//     (0, 0)
-//
-//     (0, 1)
-//     (1, 0)
-//     (1, 1)
-//
-//     (0, 2)
-//     (1, 2)
-//     (2, 0)
-//     (2, 1)
-//     (2, 2)
-//
-//     ...
-func explore(c chan<- try, maxCommitBehind int) {
-	for max := 0; ; max++ {
-		for i := 0; i <= max-1 && i <= maxCommitBehind; i++ {
-			c <- try{i, max}
-		}
-		if max <= maxCommitBehind {
-			for j := 0; j <= max; j++ {
-				c <- try{max, j}
-			}
+// explore yields a sequence of try values with increasing nonces
+func explore(c chan<- try) {
+	var t try
+	for {
+		t.nonce++
+		c <- t
+		if t.nonce == 0 {
+			break
 		}
 	}
-}
-
-// date is a git date.
-type date struct {
-	n  int64 // unix seconds
-	tz string
-}
-
-func (d date) String() string { return fmt.Sprintf("%d %s", d.n, d.tz) }
-
-// getDate parses out a date from a git header (or blob with a header
-// following the size and null byte). It returns the date and index
-// that the unix seconds begins at within h.
-func getDate(h []byte, rx *regexp.Regexp) (d date, idx int) {
-	m := rx.FindSubmatchIndex(h)
-	if m == nil {
-		log.Fatalf("Failed to match %s in %q", rx, h)
-	}
-	v := string(h[m[2]:m[3]])
-	space := strings.Index(v, " ")
-	if space < 0 {
-		log.Fatalf("unexpected date %q", v)
-	}
-	n, err := strconv.ParseInt(v[:space], 10, 64)
-	if err != nil {
-		log.Fatalf("unexpected date %q", v)
-	}
-	return date{n, v[space+1:]}, m[2]
-}
-
-func curHash() string {
-	all, err := exec.Command("git", "rev-parse", "HEAD").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	h := string(all)
-	if i := strings.Index(h, "\n"); i > 0 {
-		h = h[:i]
-	}
-	return h
+	close(c)
 }
 
 // hexInPlace takes a slice of binary data and returns the same slice with double
@@ -222,4 +67,136 @@ func hexInPlace(v []byte) []byte {
 		h[i*2+1] = hex[b&0xf]
 	}
 	return h
+}
+
+func fixHeader(obj []byte) []byte {
+	obj = obj[bytes.IndexByte(obj, '\x00')+1:]
+	obj = []byte(fmt.Sprintf("commit %d\x00%s", len(obj), obj))
+	return obj
+}
+
+const nonceHeader = "\nnonce "
+
+func addOrFindNonce(obj []byte) ([]byte, int) {
+	if !bytes.Contains(obj, []byte(nonceHeader)) {
+		i := bytes.Index(obj, []byte("\n\n"))
+		header := obj[:i]
+		footer := obj[i:]
+		obj = []byte(fmt.Sprintf("%s%s%s", header, nonceHeader, footer))
+		obj = fixHeader(obj)
+	}
+	i := bytes.Index(obj, []byte(nonceHeader))
+	for obj[i+1] != '\n' {
+		i++
+	}
+	return obj, i
+}
+
+func growNonce(obj []byte) []byte {
+	i := bytes.Index(obj, []byte(nonceHeader)) + len(nonceHeader)
+	newObj := make([]byte, 0, len(obj)+1)
+	newObj = append(newObj, obj[:i]...)
+	newObj = append(newObj, '0')
+	newObj = append(newObj, obj[i:]...)
+	newObj = fixHeader(newObj)
+	return newObj
+}
+
+func bruteForce(obj []byte, winner chan<- []byte, possibilities <-chan try, done <-chan struct{}) {
+	const nonceChars = "0123456789"
+	var nonceIdx int
+	obj, nonceIdx = addOrFindNonce(obj)
+
+	s1 := sha1.New()
+	hexBuf := make([]byte, 0, sha1.Size*2)
+
+	for t := range possibilities {
+		select {
+		case <-done:
+			return
+		default:
+		writeNonce:
+			i := nonceIdx
+			nonce := t.nonce
+			for nonce > 0 {
+				if obj[i] == ' ' {
+					obj = growNonce(obj)
+					nonceIdx++
+					goto writeNonce
+				}
+				obj[i] = nonceChars[nonce%len(nonceChars)]
+				nonce /= len(nonceChars)
+				i--
+			}
+
+			s1.Reset()
+			s1.Write(obj)
+			if !re.Match(hexInPlace(s1.Sum(hexBuf[:0]))) {
+				continue
+			}
+
+			winner <- obj
+			return
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	runtime.GOMAXPROCS(*cpu)
+
+	var err error
+	re, err = regexp.Compile(*pattern)
+	if err != nil {
+		log.Fatalf("Pattern %q is not a valid regexp: %s", err)
+	}
+
+	head, err := exec.Command("git", "rev-parse", "HEAD").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if re.MatchString(strings.TrimSpace(string(head))) {
+		fmt.Println("gitbrute:", strings.TrimSpace(string(head)), "(already matches)")
+		return
+	}
+
+	obj, err := exec.Command("git", "cat-file", "-p", "HEAD").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	obj = fixHeader(obj)
+
+	possibilities := make(chan try, 512)
+	go explore(possibilities)
+
+	winner := make(chan []byte)
+	done := make(chan struct{})
+
+	for i := 0; i < *cpu; i++ {
+		buf := make([]byte, len(obj))
+		copy(buf, obj)
+		go bruteForce(buf, winner, possibilities, done)
+	}
+
+	w := <-winner
+	close(done)
+
+	var hash bytes.Buffer
+	cmd := exec.Command("git", "hash-object", "-t", "commit", "-w", "--stdin")
+	cmd.Stdout = &hash
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = bytes.NewBuffer(w[bytes.IndexByte(w, '\x00')+1:])
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("hash-object: %v", err)
+	}
+
+	cmd = exec.Command("git", "update-ref", "HEAD", strings.TrimSpace(hash.String()))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("update-ref: %v", err)
+	}
+
+	fmt.Println("gitbrute:", strings.TrimSpace(hash.String()))
 }
